893ea16b83807772ec9e8b197c731758df103f10
john1884 <john.bothwell1884@gmail.com>
2024-01-19 16:26:38 +0000

    changes to sidebar

--- application/views/admin/authentication/sidebar.php
+++ application/views/admin/authentication/sidebar.php
@@ -1,7 +1,7 @@
 <div class="sidebar-l col-12 col-xl-4 order-md-first">
     <div class="box-left">
         <div class="logo">
-            <a class="logo-alt-1" href="/">&nbsp;</a>
+            <a href="/"><img src="/assets/images/logo-white.png" width="663" height="147" loading="lazy"></a>
         </div>
         <div class="box-pattern">
             <div class="decor-1">&nbsp;</div>
